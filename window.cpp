376101ae919d59417115f0dcacd03ff118f69a96
Simon G Hadfield <simonguyhadfield@gmail.com>
2023-08-03 13:11:14 +0100

    beginning of shader abstraction

--- window.cpp
+++ window.cpp
@@ -3,6 +3,7 @@
 #include "Renderer.h"
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
+#include "VertexArray.h"
 
 struct ShaderProgramSource
 {
@@ -135,10 +136,13 @@ int CreateWindow(bool displayState) {
         GLCall(glBindVertexArray(vao));
 
         //create buffer
+        VertexArray va;
         VertexBuffer vb(positions, 4 * 2 * sizeof(float));
 
-        GLCall(glEnableVertexAttribArray(0));
-        GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
+        VertexBufferLayout layout;
+        layout.Push<float>(2);
+        va.AddBuffer(vb,layout);
+
 
         // create index buffer
         IndexBuffer ib(indices, 6);
@@ -171,9 +175,8 @@ int CreateWindow(bool displayState) {
             GLCall(glUseProgram(shader));
             GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
 
-            GLCall(glBindVertexArray(vao));
+            va.Bind();
             ib.Bind();
-
 
             GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
